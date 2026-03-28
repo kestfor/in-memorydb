2bc00e5a53dd6c6cd08b5bf5045146933bdb526c
kestfor <anzhinilya2@gmail.com>
2026-03-28 21:06:55 +0700

    upd

--- cmd/grpc/app/server.go
+++ cmd/grpc/app/server.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 
-	"github.com/kestfor/in-memorydb/api/lume"
+	lume "github.com/kestfor/in-memorydb/api/lume"
 	"github.com/kestfor/in-memorydb/pkg/storage"
 
 	"github.com/golang/protobuf/ptypes/empty"
